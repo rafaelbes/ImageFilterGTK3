bb82d41afee153c26c60fdfdd6397d1f2b071e91
leao3 <jose@ubuntu.ubuntu-domain>
2018-11-15 11:19:09 -0300

    linhas e colunas

--- meufiltro.c
+++ meufiltro.c
@@ -40,19 +40,32 @@ Imagem meuFiltro(Imagem origem) {
 	}
 	for(j = 0; j < destino.w; j++) {
 		for(i = 0; i < destino.h; i=i+nivel) {
-			
-				destino.m[i][j][0] = 0;
-				destino.m[i][j][1] = 0;
-				destino.m[i][j][2] = 0;
-			
+			int cor1=destino.m[i][j][0];
+			int cor2=destino.m[i][j][0];
+			int cor3=destino.m[i][j][0];
+			destino.m[i][j][0] = 0;
+			destino.m[i][j][1] = 0;
+			destino.m[i][j][2] = 0;
+			for(int c=i; c<i+nivel; c++){
+				destino.m[c][j][0] = cor1;
+				destino.m[c][j][1] = cor2;
+				destino.m[c][j][2] = cor2;
+			}
 		}
 	}
 	for(j = 0; j < destino.w; j=j+nivel) {
 		for(i = 0; i < destino.h; i++) {
-			
-				destino.m[i][j][0] = 0;
-				destino.m[i][j][1] = 0;
-				destino.m[i][j][2] = 0;
+			int cor1=destino.m[i][j][0];
+			int cor2=destino.m[i][j][0];
+			int cor3=destino.m[i][j][0];
+			destino.m[i][j][0] = 0;
+			destino.m[i][j][1] = 0;
+			destino.m[i][j][2] = 0;
+			for(int c=j; c<j+nivel; c++){
+				destino.m[i][c][0] = cor1;
+				destino.m[i][c][1] = cor2;
+				destino.m[i][c][2] = cor2;
+			}
 			
 		}
 	}
