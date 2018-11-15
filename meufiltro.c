456dd08bd2ce26fcae4735c90ebc50a44dcfb77e
leao3 <jose@ubuntu.ubuntu-domain>
2018-11-14 22:18:44 -0300

    criando um grid

--- meufiltro.c
+++ meufiltro.c
@@ -20,7 +20,7 @@ Imagem meuFiltro(Imagem origem) {
 	int i, j;
 	Imagem destino = alocarImagem(origem);
 	int nivel = (int) gtk_range_get_value(GTK_RANGE(widgetControleNivel));
-	int ch1, ch2, ch3;
+	/*int ch1, ch2, ch3;
 
 	ch1 = 0;
 	ch2 = 1;
@@ -30,22 +30,30 @@ Imagem meuFiltro(Imagem origem) {
 		ch2 = (ch1+1+rand()%2)%3;
 		ch3 = 3 - ch2 - ch1;
 	}
-
+	*/
 	for(j = 0; j < destino.w; j++) {
 		for(i = 0; i < destino.h; i++) {
-			int x = j - nivel + rand()%(2*nivel+1);
-			int y = i - nivel + rand()%(2*nivel+1);
-			if(x < 0)
-				x = 0;
-			if(y < 0)
-				y = 0;
-			if(x >= destino.w)
-				x = destino.w - 1;
-			if(y >= destino.h)
-				y = destino.h - 1;
-			destino.m[i][j][0] = origem.m[y][x][ch1];
-			destino.m[i][j][1] = origem.m[y][x][ch2];
-			destino.m[i][j][2] = origem.m[y][x][ch3];
+			destino.m[i][j][0] = origem.m[i][j][0];
+			destino.m[i][j][1] = origem.m[i][j][1];
+			destino.m[i][j][2] = origem.m[i][j][2];
+		}
+	}
+	for(j = 0; j < destino.w; j++) {
+		for(i = 0; i < destino.h; i=i+nivel) {
+			
+				destino.m[i][j][0] = 0;
+				destino.m[i][j][1] = 0;
+				destino.m[i][j][2] = 0;
+			
+		}
+	}
+	for(j = 0; j < destino.w; j=j+nivel) {
+		for(i = 0; i < destino.h; i++) {
+			
+				destino.m[i][j][0] = 0;
+				destino.m[i][j][1] = 0;
+				destino.m[i][j][2] = 0;
+			
 		}
 	}
 	return destino;
